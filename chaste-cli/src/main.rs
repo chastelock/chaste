8acbbe2f07b47ccd70cd6ccb05abd4053d6e04f1
LN Liberda <lauren@selfisekai.rocks>
2024-12-19 23:25:17 +0100

    cli: Chaste, why?

--- chaste-cli/src/main.rs
+++ chaste-cli/src/main.rs
@@ -1,6 +1,27 @@
 // SPDX-FileCopyrightText: 2024 The Chaste Authors
 // SPDX-License-Identifier: Apache-2.0 OR BSD-2-Clause
 
-fn main() {
-    todo!();
+use anyhow::Result;
+use argh::FromArgs;
+
+mod why;
+
+#[derive(FromArgs)]
+/// Chaste.
+struct Args {
+    #[argh(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum Subcommand {
+    Why(why::Why),
+}
+
+fn main() -> Result<()> {
+    let args: Args = argh::from_env();
+    match args.subcommand {
+        Subcommand::Why(why) => why::run(why),
+    }
 }
---
+++ chaste-cli/src/why.rs
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: 2024 The Chaste Authors
+// SPDX-License-Identifier: Apache-2.0 OR BSD-2-Clause
+
+use anyhow::{Context, Result};
+use argh::FromArgs;
+use chaste::{Dependency, PackageID};
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "why")]
+/// Why is this package here?
+pub struct Why {
+    #[argh(positional)]
+    /// package name
+    package_name: String,
+}
+
+fn permute<'a, G>(preceding: Vec<&'a Dependency>, generator: G) -> Vec<Vec<&'a Dependency>>
+where
+    G: Fn(PackageID) -> Vec<&'a Dependency> + Clone,
+{
+    let last = preceding.last().unwrap();
+    let generated = generator(last.from);
+    match generated.len() {
+        0 => vec![preceding],
+        1 => {
+            let mut res = preceding;
+            res.push(generated.first().unwrap());
+            permute(res, generator)
+        }
+        _ => {
+            let mut res = Vec::new();
+            for g in generated {
+                let mut branch = preceding.clone();
+                branch.push(g);
+                res.extend(permute(branch, generator.clone()));
+            }
+            res
+        }
+    }
+}
+
+pub fn run(sub: Why) -> Result<()> {
+    let cwd = std::env::current_dir()?;
+    let chastefile = chaste::from_root_path(&cwd)
+        .with_context(|| format!("Could not parse the lockfile from {cwd:?}"))?;
+
+    let packages = chastefile
+        .packages_with_ids()
+        .into_iter()
+        .filter(|(_, pkg)| pkg.name().is_some_and(|n| n == sub.package_name));
+    let mut permutations = Vec::new();
+    for (current_pid, _package) in packages {
+        for dep in chastefile.package_dependents(current_pid) {
+            permutations.extend(permute(vec![dep], |pid| chastefile.package_dependents(pid)));
+        }
+    }
+    for mut permut in permutations {
+        permut.reverse();
+        let initial_pkg = chastefile.package(permut.first().unwrap().from);
+        print!("{:?}", initial_pkg.name().map(|n| n.to_string()));
+        for d in permut {
+            let pkg = chastefile.package(d.on);
+            print!(" -{:?}-> {:?}", d.kind, pkg.name().map(|n| n.to_string()));
+        }
+        println!();
+    }
+
+    Ok(())
+}
