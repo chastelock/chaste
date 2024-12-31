15efc0548e7d258eaf31275752178ad8b92c335c
LN Liberda <lauren@selfisekai.rocks>
2024-12-31 20:03:40 +0100

    cli(why): Test against a circular dependency tree

---
+++ chaste-cli/tests/why.rs
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2024 The Chaste Authors
+// SPDX-License-Identifier: Apache-2.0 OR BSD-2-Clause
+
+use anyhow::Result;
+use assert_cmd::Command;
+
+#[test]
+fn npm_v3_infinite_recursion() -> Result<()> {
+    Command::cargo_bin("chaste")?
+        .args(["why", "@chastelock/recursion-a"])
+        .current_dir("test_workspaces/npm_v3_infinite_recursion")
+        .assert()
+        .success()
+        .stdout("@chastelock/testcase -Dependency-> @chastelock/recursion-a\n");
+
+    Command::cargo_bin("chaste")?
+        .args(["why", "@chastelock/recursion-b"])
+        .current_dir("test_workspaces/npm_v3_infinite_recursion")
+        .assert()
+        .success()
+        .stdout("@chastelock/testcase -Dependency-> @chastelock/recursion-a -Dependency-> @chastelock/recursion-b\n");
+
+    Ok(())
+}
+
+#[test]
+fn yarn_v1_infinite_recursion() -> Result<()> {
+    Command::cargo_bin("chaste")?
+        .args(["why", "@chastelock/recursion-a"])
+        .current_dir("test_workspaces/yarn_v1_infinite_recursion")
+        .assert()
+        .success()
+        .stdout("@chastelock/testcase -Dependency-> @chastelock/recursion-a\n");
+
+    Command::cargo_bin("chaste")?
+        .args(["why", "@chastelock/recursion-b"])
+        .current_dir("test_workspaces/yarn_v1_infinite_recursion")
+        .assert()
+        .success()
+        .stdout("@chastelock/testcase -Dependency-> @chastelock/recursion-a -Dependency-> @chastelock/recursion-b\n");
+
+    Ok(())
+}
