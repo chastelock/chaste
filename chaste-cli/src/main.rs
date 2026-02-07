ced85db75ae532f133431f31b70b2e90b78f0cce
LN Liberda <lauren@selfisekai.rocks>
2026-02-07 05:45:38 +0100

    cli: Add --cwd for overriding the default root dir

--- chaste-cli/src/main.rs
+++ chaste-cli/src/main.rs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 The Chaste Authors
 // SPDX-License-Identifier: Apache-2.0 OR BSD-2-Clause
 
+use std::path::PathBuf;
+
 use anyhow::{Context as _, Result};
 use argh::FromArgs;
 
@@ -21,6 +23,10 @@ struct Args {
     #[argh(option, from_str_fn(implem_from_name))]
     /// implementation whose lockfile should be checked
     implem: Option<chaste::Implementation>,
+
+    #[argh(option)]
+    /// directory to be checked
+    cwd: Option<PathBuf>,
 }
 
 #[derive(FromArgs)]
@@ -33,7 +39,10 @@ enum Subcommand {
 fn main() -> Result<()> {
     let args: Args = argh::from_env();
 
-    let cwd = std::env::current_dir()?;
+    let cwd = match args.cwd {
+        Some(p) => p,
+        None => std::env::current_dir()?,
+    };
     let chastefile = if let Some(implem) = args.implem {
         chaste::from_root_path_with_implementation(&cwd, implem)
     } else {
